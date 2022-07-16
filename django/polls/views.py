9adb72fe13e5cc59d55600855a8c1646f90248de
IcyAltair <muratov.syu@edu.spbstu.ru>
2022-07-16 15:34:33 +0300

    first merge

--- django/polls/views.py
+++ django/polls/views.py
@@ -21,7 +21,8 @@ def test_1_view(request):
             report.tn_ved = form.cleaned_data["tn_ved"]
             report.tech_req = form.cleaned_data["tech_req"]
             report.group_prod = form.cleaned_data["group_prod"]
-            return render(request, "map.html")
+            return render(request, 'map.html',
+                          context={'plot_div': map_view()})
         elif form.is_valid() != True:
             sys.stdout.write(json.dumps(form.errors, indent=4))
     else:
@@ -44,47 +45,8 @@ def test_2_view(request):
             report = TwoActivity()
             report.code = form.cleaned_data["code"]
             report.common_naming = form.cleaned_data["common_naming"]
-
-            x = [i for i in range(-10, 11)]
-            y1 = [3*i for i in x]
-            y2 = [i**2 for i in x]
-            y3 = [10*abs(i) for i in x]
-
-            # List of graph objects for figure.
-            # Each object will contain on series of data.
-            graphs = []
-
-            # Adding linear plot of y1 vs. x.
-            graphs.append(
-                go.Scatter(x=x, y=y1, mode='lines', name='Line y1')
-            )
-
-            # Adding scatter plot of y2 vs. x.
-            # Size of markers defined by y2 value.
-            graphs.append(
-                go.Scatter(x=x, y=y2, mode='markers', opacity=0.8,
-                        marker_size=y2, name='Scatter y2')
-            )
-
-            # Adding bar plot of y3 vs x.
-            graphs.append(
-                go.Bar(x=x, y=y3, name='Bar y3')
-            )
-
-            # Setting layout of the figure.
-            layout = {
-                'title': 'Title of the figure',
-                'xaxis_title': 'X',
-                'yaxis_title': 'Y',
-                'height': 420,
-                'width': 560,
-            }
-
-            # Getting HTML needed to render the plot.
-            plot_div = plot({'data': graphs, 'layout': layout},
-                            output_type='div')
             return render(request, 'map.html',
-                          context={'plot_div': plot_div})
+                          context={'plot_div': map_view()})
         elif form.is_valid() != True:
             sys.stdout.write(json.dumps(form.errors, indent=4))
     else:
@@ -94,13 +56,34 @@ def test_2_view(request):
     return render(request, "test_2.html", context)
 
 
-@ensure_csrf_cookie
-def map_view(request):
-    px.set_mapbox_access_token(open(".mapbox_token").read())
-    df = px.data.carshare()
+def map_view():
+    x = [i for i in range(-10, 11)]
+    y1 = [3*i for i in x]
+    y2 = [i**2 for i in x]
+    y3 = [10*abs(i) for i in x]
+
+    # List of graph objects for figure.
+    # Each object will contain on series of data.
     graphs = []
-    graphs.append(px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon",     color="peak_hour", size="car_hours",
-                                    color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10))
+
+    # Adding linear plot of y1 vs. x.
+    graphs.append(
+        go.Scatter(x=x, y=y1, mode='lines', name='Line y1')
+    )
+
+    # Adding scatter plot of y2 vs. x.
+    # Size of markers defined by y2 value.
+    graphs.append(
+        go.Scatter(x=x, y=y2, mode='markers', opacity=0.8,
+                   marker_size=y2, name='Scatter y2')
+    )
+
+    # Adding bar plot of y3 vs x.
+    graphs.append(
+        go.Bar(x=x, y=y3, name='Bar y3')
+    )
+
+    # Setting layout of the figure.
     layout = {
         'title': 'Title of the figure',
         'xaxis_title': 'X',
@@ -109,8 +92,8 @@ def map_view(request):
         'width': 560,
     }
 
+    # Getting HTML needed to render the plot.
     plot_div = plot({'data': graphs, 'layout': layout},
                     output_type='div')
 
-    return render(request, 'map.html',
-                  context={'plot_div': plot_div})
+    return plot_div
